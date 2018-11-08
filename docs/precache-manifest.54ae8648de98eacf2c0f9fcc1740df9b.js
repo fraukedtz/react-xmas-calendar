9aae51ae566443aea098efbb42841116b33b5cf2
Frauke <frauke@MacBook-Pro-84.local>
2018-11-08 17:06:44 +0100

    add first version of calendar

---
+++ docs/precache-manifest.54ae8648de98eacf2c0f9fcc1740df9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3abaf61723b3c2425b12",
+    "url": "/static/js/main.3abaf617.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "3abaf61723b3c2425b12",
+    "url": "/static/css/main.c5badd56.chunk.css"
+  },
+  {
+    "revision": "f0090f0161a4b473e2bac97bdc35acb9",
+    "url": "/index.html"
+  }
+];
